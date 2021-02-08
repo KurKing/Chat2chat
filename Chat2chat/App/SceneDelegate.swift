1ab7bcf7963aac33161630a94d66f729b77c66e0
KurKing <lyosha.kurkin@gmail.com>
2021-02-08 21:29:37 +0200

    initial commit

---
+++ Chat2chat/App/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  Chat2chat
+//
+//  Created by Oleksiy on 08.02.2021.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+    
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    }
+
+}
+
---
+++ Chat2chatTests/Chat2chatTests.swift
@@ -0,0 +1,13 @@
+//
+//  Chat2chatTests.swift
+//  Chat2chatTests
+//
+//  Created by Oleksiy on 08.02.2021.
+//
+
+import XCTest
+@testable import Chat2chat
+
+class Chat2chatTests: XCTestCase {
+
+}
